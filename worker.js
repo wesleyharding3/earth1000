5daacb61da201a1d15be88bdbb0aa582361114a3
wesleyharding3 <wesleyharding3@gmail.com>
2026-02-17 03:22:02 -0800

    new

--- worker.js
+++ worker.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
+
 const fetchFeeds = require("./fetcher");
 
 async function run() {
+
+  console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
   await fetchFeeds();
   process.exit(0);
+
 }
 
 run();
