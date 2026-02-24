98a7070c1a0213082f65dae37d9827b0659e5799
wesleyharding3 <wesleyharding3@gmail.com>
2026-02-24 13:10:09 -0800

    new

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get("/api/news/country/:countryId", async (req, res) => {
 
     const result = await pool.query(
       `SELECT 
-        a.id, a.title, a.url, a.summary, a.image_url, a.published_at,
+        a.id, a. translated_title, a.title, a.url, a.translated_summary, a.summary, a.image_url, a.published_at,
         s.name AS source_name, s.site_url,
         co.iso_code
       FROM news_articles a
