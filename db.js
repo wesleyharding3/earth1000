36b2108093b559d1b102a35c923f3695bed6712d
wesleyharding3 <wesleyharding3@gmail.com>
2026-02-24 19:21:14 -0800

    new

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
-// db.js
 const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  connectionTimeoutMillis: 10000,
+  idleTimeoutMillis: 30000,
+  max: 5
+});
+
+pool.on('error', (err) => {
+  console.error('🚨 Unexpected DB pool error:', err);
 });
 
 module.exports = pool;
