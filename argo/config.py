4b6bc68387466b9b06ee26aca1c8cd61f134f447
wesleyharding3 <wesleyharding3@gmail.com>
2026-03-04 13:10:22 -0800

    new

--- argo/config.py
+++ argo/config.py
@@ -2,5 +2,11 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Stable public CSV dataset
-DATA_URL = "https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv"
+# Copernicus global ocean temperature dataset (via ERDDAP mirror)
+COPERNICUS_URL = (
+"https://erddap.ifremer.fr/erddap/griddap/"
+"GLOBAL_ANALYSISFORECAST_PHY_001_024.csv?"
+"thetao[(2024-01-01T00:00:00Z)]"
+"[(0.494025):1:(89.50598)]"
+"[(-179.5):1:(179.5)]"
+)
