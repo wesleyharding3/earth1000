c0dfb9363b57b6fb8c0a96fc24bf9b53a9643080
wesleyharding3 <wesleyharding3@gmail.com>
2026-03-04 12:39:47 -0800

    new

--- argo/config.py
+++ argo/config.py
@@ -3,5 +3,9 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 SST_URL = (
-"https://downloads.psl.noaa.gov/Datasets/noaa.oisst.v2.highres/sst.day.mean.nc"
+"https://coastwatch.pfeg.noaa.gov/erddap/griddap/"
+"ncdcOisst21Agg_LonPM180.csv?"
+"sst[(2024-01-01T00:00:00Z)]"
+"[( -89.875):1:(89.875)]"
+"[( -179.875):1:(179.875)]"
 )
