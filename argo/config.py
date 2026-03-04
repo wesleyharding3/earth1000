26215eab6768b6c0c4ced76302130273141cbb5a
wesleyharding3 <wesleyharding3@gmail.com>
2026-03-04 13:25:43 -0800

    new

--- argo/config.py
+++ argo/config.py
@@ -2,8 +2,8 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Stable ERDDAP ocean temperature dataset
 DATA_URL = (
-"https://coastwatch.pfeg.noaa.gov/erddap/griddap/ncdcOisst21Agg_LonPM180.csv?"
+"https://coastwatch.pfeg.noaa.gov/erddap/griddap/"
+"ncdcOisst21Agg_LonPM180.csv?"
 "sst[(2024-01-01T00:00:00Z)][(-89):1:(89)][(-179):1:(179)]"
 )
