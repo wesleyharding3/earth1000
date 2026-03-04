7e73dd3b43a833d1866f3b3de8ae9d50ffb9175c
wesleyharding3 <wesleyharding3@gmail.com>
2026-03-04 13:31:08 -0800

    new

--- argo/config.py
+++ argo/config.py
@@ -2,8 +2,6 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATA_URL = (
-"https://coastwatch.pfeg.noaa.gov/erddap/griddap/"
-"ncdcOisst21Agg_LonPM180.csv?"
-"sst[(2024-01-01T00:00:00Z)][(-89):1:(89)][(-179):1:(179)]"
-)
+DATASET_ID = "cmems_mod_glo_phy-thetao_anfc_0.083deg_PT6H-i"
+
+VARIABLE = "thetao"
