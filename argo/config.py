7db35a62b2611566a1e19d6f431ebc8e62feab27
wesleyharding3 <wesleyharding3@gmail.com>
2026-03-04 11:51:32 -0800

    new

--- argo/config.py
+++ argo/config.py
@@ -3,6 +3,5 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 ARGO_URL = (
-"https://erddap.ucsd.edu/erddap/tabledap/ArgoFloats.csv?"
-"platform_number,time,latitude,longitude,pressure,temperature,salinity"
+"https://data-argo.ifremer.fr/argo_synthetic-profile_index.txt"
 )
