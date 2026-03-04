2d829e50950b7c25792820aca737b11ae87528cb
wesleyharding3 <wesleyharding3@gmail.com>
2026-03-04 15:20:29 -0800

    new

--- argo/config.py
+++ argo/config.py
@@ -2,4 +2,4 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATA_URL = "https://psl.noaa.gov/data/correlation/sst.data"
+DATA_URL = "https://raw.githubusercontent.com/datasets/sea-surface-temperature/master/data/monthly.csv"
