c1f7256c973497e317c2148f674b89ac9b54c622
wesleyharding3 <wesleyharding3@gmail.com>
2026-03-04 14:59:57 -0800

    news

--- argo/config.py
+++ argo/config.py
@@ -2,8 +2,4 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATA_URL = (
-"https://coastwatch.pfeg.noaa.gov/erddap/griddap/"
-"ncdcOisst21Agg_LonPM180.csv?"
-"sst[(2024-01-01T00:00:00Z)][(-89):1:(89)][(-179):1:(179)]"
-)
+DATA_URL = "https://psl.noaa.gov/data/correlation/sst.data"
