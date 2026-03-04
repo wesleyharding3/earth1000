f449694a3dd17697455ccb59fb41f1455f6be642
wesleyharding3 <wesleyharding3@gmail.com>
2026-03-04 12:46:41 -0800

    new

--- argo/config.py
+++ argo/config.py
@@ -2,10 +2,5 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-SST_URL = (
-"https://coastwatch.pfeg.noaa.gov/erddap/griddap/"
-"ncdcOisst21Agg_LonPM180.csv?"
-"sst[(2024-01-01T00:00:00Z)]"
-"[( -89.875):1:(89.875)]"
-"[( -179.875):1:(179.875)]"
-)
+# Stable public CSV dataset
+DATA_URL = "https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv"
