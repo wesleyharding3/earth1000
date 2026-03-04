e3b0967eac634c56f1f18f7ceaf1944654770079
wesleyharding3 <wesleyharding3@gmail.com>
2026-03-04 13:43:10 -0800

    new

--- argo/config.py
+++ argo/config.py
@@ -2,6 +2,7 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Copernicus global ocean temperature dataset
 DATASET_ID = "cmems_mod_glo_phy-thetao_anfc_0.083deg_PT6H-i"
 
 VARIABLE = "thetao"
